1531265adfea1d11b82706609878cb226d0b3587
bojantomic <tomicb@fon.rs>
2019-04-23 14:09:45 +0200

    Izmena mala

--- src/main/Main6.java
+++ src/main/Main6.java
@@ -1,10 +1,14 @@
 package main;
 
 import java.io.FileReader;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonElement;
+
 
 public class Main6 {
 	
@@ -13,6 +17,11 @@ public class Main6 {
 			Gson gson = new GsonBuilder().setPrettyPrinting().create();
 			
 			JsonObject m = gson.fromJson(in, JsonObject.class);
+			
+			Set<Entry<String,JsonElement>> es = m.entrySet();
+			
+			for(Entry<String,JsonElement> entry: es)
+				System.out.println(entry.getKey()+ " " + entry.getValue().toString());
 			
 			double rating = m.get("rating").getAsDouble() - 1;
 			
